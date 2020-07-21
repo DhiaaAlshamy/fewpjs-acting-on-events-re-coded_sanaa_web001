53629a0f8a31d51472615b2045ecb5cb3bb74033
Dhiaa Alshamy <dhodhof@gmail.com>
2020-07-21 23:59:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // Your code here
 let dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#FF69B4";
+document.addEventListener("keydown", function(e) {
+  console.log(e.key);
+});
