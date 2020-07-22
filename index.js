655a64279b521478735b525c705a669e9bef9e32
Dhiaa Alshamy <dhodhof@gmail.com>
2020-07-22 00:05:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,10 @@ function moveDodgerLeft() {
  
   if (left > 0) {
     dodger.style.left = `${left - 1}px`;
+    document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
   }
 }
